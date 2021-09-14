63e618bfbbd77d7ac791029a0a93a9a3c1f706ee
BartdeBoer2001 <bartdb2001@gmail.com>
2021-09-14 15:14:50 +0200

    grid setup

--- grid.js
+++ grid.js
@@ -8,6 +8,15 @@ canvas.width = width;
 canvas.height = height;
 
 context.moveTo(0,0);
+context.lineTo(0,400);
+context.lineTo(400,400);
+context.lineTo(400, 0);
+context.lineTo(0,0);
 context.lineTo(0,100);
-context.lineTo(100,0);
+context.lineTo(400, 100);
+context.lineTo(400, 200);
+context.lineTo(0, 200);
+context.lineTo(0, 300);
+context.lineTo(400, 300);
+
 context.stroke();
