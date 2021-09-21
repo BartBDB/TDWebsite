6c6ca398cb527c2286871b3dfc76131a1d5a7be1
BartdeBoer2001 <bartdb2001@gmail.com>
2021-09-21 11:56:12 +0200

    grid setup

--- grid.js
+++ grid.js
@@ -1,22 +1,15 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+const container = document.getElementById("container");
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+function makeGrid(rows, cols) {
+  container.style.setProperty('grid-row', rows);
+  container.style.setProperty('grid-column', cols);
+  for (i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    cell.innerText = (i + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
 
-canvas.width = width;
-canvas.height = height;
 
-context.moveTo(0,0);
-context.lineTo(0,400);
-context.lineTo(400,400);
-context.lineTo(400, 0);
-context.lineTo(0,0);
-context.lineTo(0,100);
-context.lineTo(400, 100);
-context.lineTo(400, 200);
-context.lineTo(0, 200);
-context.lineTo(0, 300);
-context.lineTo(400, 300);
 
-context.stroke();
+makeGrid(Box1, Box2);
