b69c1d6be93bfacc2b74ee01568a99794cb441d1
BartdeBoer2001 <bartdb2001@gmail.com>
2021-09-28 11:43:44 +0200

    icon, working grid generation

--- grid.js
+++ grid.js
@@ -6,8 +6,15 @@ button.addEventListener("click", ()=>{
   
     let divHolder = document.getElementById('grid')
 
-    let tilesize = 10;
+    let tilesize = 30;
+    let borderSize = 2;
     let tileTotal = height * width;
+
+    for (let i = divHolder.children.length-1; i > 0; i--)
+    {
+        divHolder.removeChild(divHolder.children[i])
+    }
+
     //console.log(tileTotal);
     for (let i = 0; i < tileTotal; i++)
     {
@@ -15,11 +22,12 @@ button.addEventListener("click", ()=>{
         tile.className = 'tile';
         tile.style.width = tilesize+'px';
         tile.style.height = tilesize+'px';
+        tile.style.borderWidth = borderSize+'px';
 
-        tile.style.left = (i % width) * tilesize + 'px';
+        tile.style.left = (i % width) * (tilesize + borderSize) + 'px';
         //console.log(newDiv.style.left);
 
-        tile.style.top = Math.floor(i / width) * tilesize + 'px';
+        tile.style.top = Math.floor(i / width) * (tilesize + borderSize) + 'px';
 
         divHolder.appendChild(tile);
     }
