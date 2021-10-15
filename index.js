6088c6a5bee99b10912e71d264233d66f8b4e098
Bart de Boer <bartdb2001@gmail.com>
2021-10-15 12:02:26 +0200

    aaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- index.js
+++ index.js
@@ -24,10 +24,11 @@ io.on('connection',(socket) => {
         io.emit('respond', msg)
         fs.writeFile('level.json', msg, (err) => { //and here is where we write to the JSON file
             if (err) throw err;
-            console.log('The file has been saved!');
-          });
+            console.log('The file has been saved!');   
+        });       
     })
 })
+
 
 server.listen(3000, ()=> {
 console.log("listenin")
