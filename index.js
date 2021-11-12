b5c28f93a1c66665f4ab628d65eb5a6fad7fd51c
Bart de Boer <bartdb2001@gmail.com>
2021-11-12 11:27:23 +0100

    heroooooooooku

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const server = http.createServer(app);
 const { Server } = require('socket.io')
 const io = new Server(server)
 
+const PORT = process.env.PORT || 5000
+
+const env = Object.assign({}, process.env, {PORT: 5000});
+
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
@@ -32,6 +36,6 @@ io.on('connection',(socket) => {
 })
 
 
-server.listen(3000, ()=> {
+server.listen(PORT, ()=> {
 console.log("listenin")
 })
